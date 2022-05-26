954a7d6ee9bc8118a186591ef828a73ab64716fa
standard-song <blacksjs@naver.com>
2022-05-26 10:13:13 +0900

    Modify : main.go delete 주석

--- main.go
+++ main.go
@@ -104,29 +104,6 @@ func main() {
 
 	})
 
-	// ===========================================================================
-	// ===========================================================================
-	// ===========================================================================
-
-	e.POST("/hello", func(c echo.Context) error {
-		var hello HelloEntity
-		err := c.Bind(&hello)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-		}
-
-		hello.Id = uuid.New()
-		err = db.Create(&hello).Error
-
-		if err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-		}
-
-		return c.JSON(http.StatusCreated, echo.Map{
-			"id": hello.Id,
-		})
-	})
-
 	content := e.Group("/content")
 	{
 		// GET
@@ -150,10 +127,6 @@ func main() {
 					return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 				}
 
-				// if video.StateLabel {
-
-				// }
-
 				return c.JSON(http.StatusOK, video)
 			})
 		content.GET("/", func(c echo.Context) error {
@@ -173,137 +146,8 @@ func main() {
 		content.POST("/:id/monitoring", controller.MarkMonotoring)
 	}
 
-	// e.GET("/hello", func(c echo.Context) error {
-	// 	var items []HelloEntity
-	// 	err := db.Find(&items).Error
-	// 	if err != nil {
-	// 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-	// 	}
-
-	// 	if len(items) == 0 {
-	// 		return c.NoContent(http.StatusNoContent)
-	// 	}
-
-	// 	return c.JSON(http.StatusOK, items)
-	// })
-
-	// e.POST("/hello", func(c echo.Context) error {
-	// 	var hello HelloEntity
-	// 	err := c.Bind(&hello)
-	// 	if err != nil {
-	// 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-	// 	}
-
-	// 	hello.Id = uuid.New()
-	// 	err = db.Create(&hello).Error
-	// 	if err != nil {
-	// 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-	// 	}
-
-	// 	return c.JSON(http.StatusCreated, echo.Map{
-	// 		"id": hello.Id,
-	// 	})
-	// })
-
-	// e.GET("/hello/:id", func(c echo.Context) error {
-	// 	var binder struct {
-	// 		HelloId uuid.UUID `param:"id"`
-	// 	}
-
-	// 	err := c.Bind(&binder)
-	// 	if err != nil {
-	// 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-	// 	}
-
-	// 	var hello HelloEntity
-	// 	err = db.First(&hello, binder.HelloId).Error
-	// 	if err == gorm.ErrRecordNotFound {
-	// 		return echo.NewHTTPError(http.StatusNotFound, "hello entity not found")
-	// 	} else if err != nil {
-	// 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-	// 	}
-
-	// 	return c.JSON(http.StatusOK, hello)
-	// })
-
-	// e.PUT("/hello/:id", func(c echo.Context) error {
-	// 	var binder struct {
-	// 		HelloId uuid.UUID `json:"-" param:"id"`
-	// 		Name    string    `json:"name"`
-	// 	}
-
-	// 	err := c.Bind(&binder)
-	// 	if err != nil {
-	// 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-	// 	}
-
-	// 	var hello HelloEntity
-
-	// 	err = db.Transaction(func(tx *gorm.DB) (err error) {
-	// 		// value update
-	// 		// business logic
-
-	// 		err = tx.Clauses(clause.Locking{
-	// 			Strength: "UPDATE",
-	// 		}).First(&hello, binder.HelloId).Error
-	// 		if err != nil {
-	// 			return
-	// 		}
-	// 		hello.Name = binder.Name
-	// 		err = tx.Save(&hello).Error
-	// 		return
-	// 	}, &sql.TxOptions{
-	// 		Isolation: sql.LevelSerializable,
-	// 	})
-
-	// 	if err == gorm.ErrRecordNotFound {
-	// 		return echo.NewHTTPError(http.StatusNotFound, "hello entity not found")
-	// 	} else if err != nil {
-	// 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-	// 	}
-
-	// 	return c.NoContent(http.StatusNoContent)
-	// })
-
-	// e.DELETE("/hello/:id", func(c echo.Context) error {
-	// 	var binder struct {
-	// 		HelloId uuid.UUID `json:"-" param:"id"`
-	// 	}
-	// 	err := c.Bind(&binder)
-	// 	if err != nil {
-	// 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-	// 	}
-
-	// 	err = db.Delete(&HelloEntity{}, binder.HelloId).Error
-	// 	if err != nil {
-	// 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-	// 	}
-
-	// 	return c.NoContent(http.StatusNoContent)
-	// })
 	e.Start(os.Getenv("SERVE_ADDR"))
 }
-
-// func getDB() *gorm.DB {
-// 	conn := os.Getenv("DB_CONN")
-// 	db, err := gorm.Open(mysql.Open(conn), &gorm.Config{})
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	sqlDB, err := db.DB()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	err = sqlDB.Ping()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	sqlDB.SetMaxIdleConns(15)
-// 	sqlDB.SetMaxOpenConns(15)
-// 	return db
-// }
 
 // hello entity
 
@@ -367,29 +211,3 @@ type DenyTagEntity struct {
 func (DenyTagEntity) TableName() string {
 	return "deny_tag"
 }
-
-// func migrate(db *gorm.DB) {
-// 	err := db.AutoMigrate(&HelloEntity{}, &DenyLogEntity{}, &VideoEntity{}, &DenyTagEntity{})
-// 	// db.Exec("ALTER TABLE deny_log ADD FOREIGN KEY (content_id) REFERENCES video(content_id);")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
-
-// const (
-// 	F_DENY string = "DENY"
-// )
-
-// func isValid(state VideoEntity) bool {
-
-// 	switch state.State {
-// 	case DENY, APPROVE, NONE:
-// 		return true
-// 	default:
-// 		return false
-// 	}
-// }
-
-// type inputBody struct {
-// 	State Videostate `json:"state" validate:"eq=APPORVE|eq=DENY|eq=NONE"`
-// }
